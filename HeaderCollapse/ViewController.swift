30cc08a84c5e91aa39c0a2aacf4d1dfde7d9011e
Humberto Dib Rayashi <rayashi@gmail.com>
2019-04-30 16:57:48 -0300

    Resizing header height with scrilling

--- HeaderCollapse/ViewController.swift
+++ HeaderCollapse/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var headerTitle: UILabel!
+    @IBOutlet weak var headerHeightConstraint: NSLayoutConstraint!
+    
+    let headerMaxHeight: CGFloat = 180
+    let headerMinHeight: CGFloat = 44 + UIApplication.shared.statusBarFrame.height
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+}
 
+extension ViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let yPosition: CGFloat = scrollView.contentOffset.y
+        let newHeaderHeight: CGFloat = headerHeightConstraint.constant - yPosition
+        
+        if newHeaderHeight > headerMaxHeight {
+            headerHeightConstraint.constant = headerMaxHeight
+        } else if newHeaderHeight < headerMinHeight {
+            headerHeightConstraint.constant = headerMinHeight
+        } else {
+            headerHeightConstraint.constant = newHeaderHeight
+            scrollView.contentOffset.y = 0
+        }
+        
+        print(yPosition)
+        
+    }
 }
 
